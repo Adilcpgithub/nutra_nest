8d658238318f82d0c28331f6dcca6826ba38d018
Adil cp <adilcp8590@gmailcom>
2024-10-18 11:22:16 +0530

    Animation almost done

--- lib/Blocs/Splash/bloc/splash_bloc.dart
+++ lib/Blocs/Splash/bloc/splash_bloc.dart
@@ -11,9 +11,8 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   }
   Future<void> _onStartAnimation(
       StartAnimationEvent event, Emitter<SplashState> emit) async {
-    // await Future.delayed(const Duration(seconds: 1));
-    emit(const SplashState(imageSize: 200, rotate: 180, opacity: 0.1));
-    // await Future.delayed(const Duration(seconds: 1));
+    emit(const SplashState(imageSize: 200, rotate: 180, opacity: 0.5));
+
     emit(const SplashState(imageSize: 280, rotate: 270, opacity: 1.0));
   }
 }
